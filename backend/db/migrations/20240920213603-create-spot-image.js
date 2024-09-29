5373baac9119fee45fd4184807513a367e25ad60
TinaK92 <tinakonopko@gmail.com>
2024-09-29 14:34:06 -0400

    fixed render issues

--- backend/db/migrations/20240920213603-create-spot-image.js
+++ backend/db/migrations/20240920213603-create-spot-image.js
@@ -49,6 +49,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'SpotImages';
-    await queryInterface.dropTable('SpotImages');
+    return queryInterface.dropTable(options);
   }
 };
